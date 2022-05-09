e7987d709e7bcb1448651a2731094e9502ffa34d
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2022-05-09 08:50:33 +0000

    small Changes

--- run.py
+++ run.py
@@ -7,11 +7,10 @@ from random import randint
 
 HIDDEN_BOARD = [[''] * 8 for x in range(8)]
 GUESS_BOARD = [[''] * 8 for x in range(8)]
+LETTERS_TO_NUMBERS = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
-letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-
-def print_board(board): 
-    print(' a b c d e f g h')
+def print_board(board):
+    print(' A B C D E F G H')
     print(' ---------------')
     row_number = 1 
     for row in board: 
