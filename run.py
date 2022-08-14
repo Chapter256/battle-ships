2f7ef8dc2f10384a9495eb288164b24e463bbea7
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2022-08-14 14:06:50 +0000

    cleaned up code

--- run.py
+++ run.py
@@ -11,9 +11,8 @@ LETTERS_TO_NUMBERS = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H
 
 def print_board(board):
     print(' A B C D E F G H')
-    print(' ---------------')
-    row_number = 1 
-    for row in board: 
+    row_number = 1
+    for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
