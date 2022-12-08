c6b8ad5ed803463c104b23e9ff347ac9d307b3b7
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2022-12-08 02:05:52 +0000

    change ship range from 10 to 5

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def print_board(board):
         row_number += 1
 
 def create_ships(board):
-    for ship in range(10):
+    for ship in range(5):
         ship_row, ship_column = randint(0, 7), randint(0, 7)
         while board[ship_row][ship_column] == 'X':
             ship_row, ship_column = randint(0, 7), randint(0, 7)
